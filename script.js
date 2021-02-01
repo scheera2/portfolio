0fd63067e934e166afb21a9b996fade465aed65d
scheera2 <scheera2@miamioh.edu>
2021-02-01 14:30:07 -0500

    reworded some pages, working on making the hope page scale more efficently with mobile

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ function showSlides() {
   setTimeout(showSlides, 5000);
 }
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+function mobileNavbar() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
